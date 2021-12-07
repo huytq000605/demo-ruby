456a02ae52e823a8cfe48624534557d72e3c3918
huytq <ygoldendarkness@gmail.com>
2021-12-07 15:43:29 +0700

    Test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'before publication' do  # (almost) plain English
+    it 'cannot have comments' do   #
+      expect { Post.create!.comments.create! }.to raise_error(ActiveRecord::RecordInvalid)  # test code
+    end
+  end
+end
+
+RSpec.describe PostsController, type: :controller do
+  describe 'PostIndex' do
+    it 'return 200' do 
+      get :index
+      expect(response).to have_http_status(200)
+    end
+  end
 end
